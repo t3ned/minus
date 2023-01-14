a0955e47fc3b2bcf24a20dd78885959894c829ca
T3NED <oss@t3ned.dev>
2023-01-14 18:25:49 +0000

    Update app bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { APP_FILTER } from "@nestjs/core";
+import { AppController } from "./app.controller";
+import { HttpExceptionFilter } from "@/filters";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [],
+	controllers: [AppController],
+	providers: [
+		{
+			provide: APP_FILTER,
+			useClass: HttpExceptionFilter,
+		},
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import "module-alias/register";
+import config from "./config/configuration";
+import { ForwardedByHeaderInterceptor } from "@/interceptors";
+import { HttpStatus, VersioningType } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as Sentry from "@sentry/node";
+import "@sentry/tracing";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+Sentry.init({
+	dsn: config.sentry.dsn,
+	tracesSampleRate: config.sentry.tracesSampleRate,
+	environment: config.app.environment,
+});
+
+const bootstrap = async () => {
+	const app = await NestFactory.create(AppModule);
+	const adapter = app.getHttpAdapter().getInstance();
+
+	adapter.set("etag", false);
+	adapter.set("x-powered-by", false);
+
+	app.enableCors({
+		origin: "*",
+		credentials: true,
+		optionsSuccessStatus: HttpStatus.NO_CONTENT,
+	});
+
+	app.enableVersioning({
+		type: VersioningType.URI,
+		prefix: "v",
+	});
+
+	app.useGlobalInterceptors(new ForwardedByHeaderInterceptor());
+
+	await app.listen(config.app.port);
+};
+
 bootstrap();
